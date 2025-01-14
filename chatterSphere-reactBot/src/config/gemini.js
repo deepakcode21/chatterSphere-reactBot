5fe204cb3781839a3da9c9864301b1e5ec0dd060
deepakcode21@gmail.com <deepakcode21@gmail.com>
2025-01-15 00:51:38 +0530

    complete dev

---
+++ chatterSphere-reactBot/src/config/gemini.js
@@ -0,0 +1,38 @@
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const MODEL_NAME = "gemini-1.0-pro";
+const API_KEY = "AIzaSyAGyLu4BlV6Z6SXllVKPtlM4HVUG4HmCGg";
+
+async function runChat(prompt) {
+
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({model: MODEL_NAME});
+    
+    const generationConfig = {
+        temperature: 1,
+        topP: 0.95,
+        topK: 40,
+        maxOutputTokens: 2048,
+        responseMimeType: "text/plain",
+    };
+
+    const chat = model.startChat({
+        generationConfig,
+        history:[
+    
+        ],
+    });
+    
+    const result = await chat.sendMessage(prompt);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+}
+
+export default runChat;
